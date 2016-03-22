e0a7c1ae95315395c79d8d425898554c833b4687
Gerhard Pegel <gpegel@gmail.com>
2016-03-22 16:34:29 +0100

    minor changes

--- example.py
+++ example.py
@@ -1,18 +1,18 @@
+### Try to connect to server
 curl -i -n -X POST --header "Content-Type: application/json" -d@- http://zabbix-server/zabbix/api_jsonrpc.php
 
+### After connection, start using Authentication
 ### Authentication
-
 {
     "jsonrpc": "2.0",
     "method": "user.login",
     "params": {
-        "user": "username",
-        "password": "password"
+        "user": " ",
+        "password": " "
     },
     "id": 1,
     "auth": null
 }
-
 
 ### Retrieving hosts
 {
@@ -29,11 +29,10 @@ curl -i -n -X POST --header "Content-Type: application/json" -d@- http://zabbix-
         ]
     },
     "id": 1,
-    "auth": "183f8e229113f5efe0fe558f86b7369b"
+    "auth": "df3f5dbfc6ec99dfe6b3061415bf86b2"
 }
 
 ### Creating a new item
-
 {
     "jsonrpc": "2.0",
     "method": "item.create",
